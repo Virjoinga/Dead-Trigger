5a6fbf7324306ab2b35d647f95c2efaaf39e9d20
Virjoinga <ethanbest51@gmail.com>
2025-04-06 17:55:58 +1200

    Update ObjectLayer.cs

--- Assets/Scripts/Assembly-CSharp/ObjectLayer.cs
+++ Assets/Scripts/Assembly-CSharp/ObjectLayer.cs
@@ -1,35 +1,40 @@
 using UnityEngine;
 
-internal static class ObjectLayer
+internal class ObjectLayer
 {
-	public static int Default;
+    public static int Default;
 
-	public static int Player;
+    public static int Player;
 
-	public static int Enemy;
+    public static int Enemy;
 
-	public static int EnemyBox;
+    public static int EnemyBox;
 
-	public static int IgnoreEnemy;
+    public static int IgnoreEnemy;
 
-	public static int IgnorePlayer;
+    public static int IgnorePlayer;
 
-	public static int IgnoreRaycast;
+    public static int IgnoreRaycast;
 
-	public static int IgnoreBullets;
+    public static int IgnoreBullets;
 
-	public static int Pumpkin;
+    public static int Pumpkin;
 
-	static ObjectLayer()
-	{
-		Default = LayerMask.NameToLayer("Default");
-		Player = LayerMask.NameToLayer("Player");
-		Enemy = LayerMask.NameToLayer("Enemy");
-		EnemyBox = LayerMask.NameToLayer("EnemyBox");
-		IgnoreEnemy = LayerMask.NameToLayer("Ignore Enemy");
-		IgnorePlayer = LayerMask.NameToLayer("Ignore Player");
-		IgnoreRaycast = LayerMask.NameToLayer("Ignore Raycast");
-		IgnoreBullets = LayerMask.NameToLayer("Ignore Bullets");
-		Pumpkin = LayerMask.NameToLayer("Pumpkin");
-	}
+
+    public static void Initialize()
+    {
+        Default = LayerMask.NameToLayer("Default");
+        Player = LayerMask.NameToLayer("Player");
+        Enemy = LayerMask.NameToLayer("Enemy");
+        EnemyBox = LayerMask.NameToLayer("EnemyBox");
+        IgnoreEnemy = LayerMask.NameToLayer("Ignore Enemy");
+        IgnorePlayer = LayerMask.NameToLayer("Ignore Player");
+        IgnoreRaycast = LayerMask.NameToLayer("Ignore Raycast");
+        IgnoreBullets = LayerMask.NameToLayer("Ignore Bullets");
+        Pumpkin = LayerMask.NameToLayer("Pumpkin");
+    }
+public void Awake()
+    {
+        ObjectLayer.Initialize();
+    }
 }
